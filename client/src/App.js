7b3d0af4f40598175572626671c2930fe942fdc6
curtislin7 <curtislin7@gmail.com>
2020-06-20 09:02:13 -0600

    init commit in remote

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       This is where everything will live.
+      </header>
+    </div>
+  );
+}
+
+export default App;
