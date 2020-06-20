2125f80c8d0a61574db1f97d11c1c6d4dd4f5d0e
curtislin7 <curtislin7@gmail.com>
2020-06-19 18:40:36 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/test_this', (req, res) => {
+    res.json('sup bitch')
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/clien/build/index.html'));
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port)
